040da8660a9d5ef6c206b2edc1a43d738660e162
Zifan Hua <zifan.hua@icloud.com>
2023-11-12 21:50:43 +0000

    Add cloudme webdav
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/webdav/cloudme.nix
+++ app/webdav/cloudme.nix
@@ -3,17 +3,17 @@
   services.davfs2.enable = true;
   services.autofs = {
     enable = true;
-    autoMaster = "/mnt/ /etc/davfs2/cloudme.auto.mount";
+    autoMaster = "/mnt/ /etc/davfs2/auto.mount";
   };
-  environment.etc."davfs2/cloudme.auto.mount" = {
+  environment.etc."davfs2/auto.mount" = {
     text = ''
-      cloudme -fstype=davfs,conf=/etc/davfs2/cloudme.conf,rw https\://webdav.cloudme.com/eawufyhaew
+      hetzner -fstype=davfs,conf=/etc/davfs2/conf,rw https\://u323536.your-storagebox.de
     '';
     mode = "0440";
   };
-  environment.etc."davfs2/cloudme.conf" = {
+  environment.etc."davfs2/conf" = {
     text = ''
-      secrets ${config.age.secrets.cloudme-webdav-secrets.path}
+      secrets ${config.age.secrets.hetzner-webdav-secrets.path}
     '';
     mode = "0440";
   };
