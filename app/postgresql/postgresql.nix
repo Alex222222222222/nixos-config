c9bcb6ec7b789145b4e8c249bcf2734df85fd7ca
Zifan Hua <zifan.hua@icloud.com>
2023-11-12 16:12:09 +0000

    Add postgresql
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/postgresql/postgresql.nix
+++ app/postgresql/postgresql.nix
@@ -11,6 +11,7 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #...
       #type database DBuser origin-address auth-method
+      local all       all                   trust
       # ipv4
       host  all      all     127.0.0.1/32   trust
       # ipv6
