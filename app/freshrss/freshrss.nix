24535fa34f9b55cd19952b14654ccd86a28681aa
Zifan Hua <zifan.hua@icloud.com>
2023-11-14 23:31:24 +0000

    Fix some file permissions
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/freshrss/freshrss.nix
+++ app/freshrss/freshrss.nix
@@ -18,11 +18,11 @@
     };
   };
 
+  users.groups.cloudflared = { };
   users.users.cloudflared = {
     group = "cloudflared";
     isSystemUser = true;
   };
-  users.groups.cloudflared = { };
 
   system.activationScripts."freshrss_cloudflare_tunnel_token" = ''
     secret=$(cat "${config.age.secrets.freshrss_tunnel_token.path}")
@@ -34,7 +34,7 @@
     # Creates /etc/nanorc
     "freshrss/freshrss_cloudflare_tunnel" = {
       text = ''
-        ${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token={{ FRESH_RSS_PASS }})
+        ${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token={{ FRESH_RSS_PASS }}
       '';
       user = "cloudflared";
       group = "cloudflared";
