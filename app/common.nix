9265ef9ea629ade8648fe238d530133556926d94
Zifan Hua <zifan.hua@icloud.com>
2023-12-19 02:27:12 +0000

    Add cloudflare warp proxy for freshrss
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/common.nix
+++ app/common.nix
@@ -9,4 +9,12 @@
     ./webdav/hetzner.nix
     ./socks5_server.nix
   ];
+
+  users.groups.cloudflared = { };
+  users.users.cloudflared = {
+    group = "cloudflared";
+    isSystemUser = true;
+    description = "cloudlfared tunnel user";
+    useDefaultShell = true;
+  };
 }
