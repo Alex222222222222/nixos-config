778f545341ff97d86e9eaacd9afe343d3b421373
Zifan Hua <zifan.hua@icloud.com>
2024-05-03 13:49:38 +0100

    Listen for localhost
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/cockroach.nix
+++ app/cockroach.nix
@@ -5,8 +5,8 @@
       enable = true;
       # open the port for the cockroachdb service
       openPorts = true;
-      # listen to all interface for intra-cluster communication
-      listen.address = "*";
+      # listen to localhost for intra-cluster communication
+      listen.address = "localhost";
       # join the cluster
       join = "chicago.tail0c3f1.ts.net:26257,euserv.tail0c3f1.ts.net:26257,server-factory-us.tail0c3f1.ts.net:26257,ihcat.tail0c3f1.ts.net:26257";
       # certs for the cluster
